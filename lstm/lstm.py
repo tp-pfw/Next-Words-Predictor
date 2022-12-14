09888c2d437aa172d829b59e729f220ddfff6702
tp-pfw <75355992+tp-pfw@users.noreply.github.com>
2022-12-14 16:09:10 -0500

    Add files via upload

--- lstm/lstm.py
+++ lstm/lstm.py
@@ -1,1 +1,73 @@
-test lstm
+from keras.preprocessing.text import Tokenizer
+import nltk
+from nltk.tokenize import word_tokenize
+import numpy as np
+import re
+from keras.utils import to_categorical
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Embedding
+from doc3 import training_doc3
+
+file = open("en_US.txt", "r", encoding = "utf8")
+lines = []
+for i in file:
+    lines.append(i)
+    
+print("The First Line: ", lines[0])
+print("The Last Line: ", lines[-1])
+
+
+data = ""
+
+for i in lines:
+    data = ' '. join(lines)
+cleaned = re.sub(r'\W+', ' ', data).lower()
+tokens = word_tokenize(cleaned)
+
+train_len = 3+1
+text_sequences = []
+for i in range(train_len,len(tokens)):
+    seq = tokens[i-train_len:i]
+    text_sequences.append(seq)
+    
+sequences = {}
+count = 1
+for i in range(len(tokens)):
+    if tokens[i] not in sequences:
+        sequences[tokens[i]] = count
+        count += 1
+        
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(text_sequences)
+sequences = tokenizer.texts_to_sequences(text_sequences) 
+
+#Collecting some information   
+vocabulary_size = len(tokenizer.word_counts)+1
+
+n_sequences = np.empty([len(sequences),train_len], dtype='int32')
+for i in range(len(sequences)):
+    n_sequences[i] = sequences[i]
+
+train_inputs = n_sequences[:,:-1]
+train_targets = n_sequences[:,-1]
+train_targets = to_categorical(train_targets, num_classes=vocabulary_size)
+seq_len = train_inputs.shape[1]
+train_inputs.shape
+#print(train_targets[0])
+
+train_targets[0]
+
+model = Sequential()
+model.add(Embedding(vocabulary_size, seq_len, input_length=seq_len))
+model.add(LSTM(50,return_sequences=True))
+model.add(LSTM(50))
+model.add(Dense(50,activation='relu'))
+model.add(Dense(vocabulary_size, activation='softmax'))
+print(model.summary())
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(train_inputs,train_targets,epochs=10,verbose=1)
+
+model.save("mymodel3.h5")
